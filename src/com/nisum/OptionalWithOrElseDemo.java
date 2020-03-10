7853f3029fb27529683d8636041ec8dfaa426cd9
vnilla <venkatakotinilla@gmail.com>
2020-03-10 16:00:31 +0530

    added new project

--- src/com/nisum/OptionalWithOrElseDemo.java
+++ src/com/nisum/OptionalWithOrElseDemo.java
@@ -13,21 +13,13 @@ public class OptionalWithOrElseDemo {
 
         System.out.println(value);
 
-        //orElseGet() will only be executed when Optional<T> is null.
 
         Optional<String> emptyOptional = Optional.ofNullable(null);
         String value1 = emptyOptional.orElse(iAmstillExecuted());
 
         System.out.println(value1);
 
-       /* Optional<String> nonEmptyOptional = Optional.of("Nisum");
-        String value = nonEmptyOptional.orElseGet(()-> "I got executed");
-        System.out.println(value);
 
-        Optional<String> emptyOptional = Optional.ofNullable(null);
-        String value1 = emptyOptional.orElseGet(()-> "I got executed");
-
-        System.out.println(value1);*/
     }
 
     public static String iAmStillExecuted(){
